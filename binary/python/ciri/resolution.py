4bc2c42d10faad8c38d6f0a3587e99c3b808d9d9
ektrah <ektrah@users.noreply.github.com>
2021-04-28 14:04:52 +0200

    Comment the steps based on the draft text
    Step 6 (If the number of "path" options in the buffer is one and the value of that option is the zero-length string, remove that option from the buffer.) is performed across all the other steps.

--- binary/python/ciri/resolution.py
+++ binary/python/ciri/resolution.py
@@ -4,22 +4,26 @@ def resolve(base, href):
   if not is_absolute(base) or not is_wellformed(href):
     return None
 
-  result = []
-
-  e = Option.FRAGMENT
+  # Determine the values of two variables, T and E, based on the
+  # first option in the sequence of options of the CRI reference to
+  # be resolved, according to Table 1.
   t = 0
+  e = Option.FRAGMENT
   for option, value in href:
     if option == Option.HOST_IP:
       e = Option.HOST_NAME
     elif option == Option.BASE_PATH:
-      e = Option.QUERY if value > 0 else Option.PATH
       t = value
+      e = Option.QUERY if t > 0 else Option.PATH
     elif option == Option.PATH:
       e = Option.QUERY
     else:
       e = option
     break
 
+  # Initialize a buffer with all the options from the base CRI where
+  # the option number is less than the value of E.
+  result = []
   for option, value in base:
     if option >= e:
       break
@@ -28,10 +32,15 @@ def resolve(base, href):
     result.append((option, value))
   _normalize_empty_path(result)
 
+  # If the value of T is greater than 0, remove the last T-many
+  # "path" options from the end of the buffer (up to the number of
+  # "path" options in the buffer).
   while t > 0 and result[-1][0] == Option.PATH:
     del result[-1]
     t -= 1
 
+  # Append all the options from the CRI reference to the buffer,
+  # excluding any "base-path" option.
   for option, value in href:
     if option > Option.PATH:
       _normalize_empty_path(result)
@@ -39,6 +48,7 @@ def resolve(base, href):
       result.append((option, value))
   _normalize_empty_path(result)
 
+  # Return the sequence of options in the buffer as the resolved CRI.
   return result
 
 def _normalize_empty_path(result):
